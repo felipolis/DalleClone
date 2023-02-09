74782f94c3ccb0ecd83902c364005c19e93f2e4b
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-02-08 21:53:23 -0300

    Create Post

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
   const [searchText, setSearchText] = useState('')
 
   return (
-    <section className='max-w-7x1 mx-auto'>
+    <section className='max-w-7xl mx-auto'>
       <div>
         <h1 className='font-extrabold text-[#222328] text-[16px]'>The Community Showcase</h1>
         <p className='mt-2 text-[#666e75] text-[14px] max-w-[500px]'>
