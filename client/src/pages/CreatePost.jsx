15a03322d1b4369b2d32ee78ba90063a1614ec9e
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-02-09 18:55:14 -0300

    Upload das imagens na nuvem

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -45,8 +45,33 @@ const CreatePost = () => {
     }
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
+    e.preventDefault()
 
+    if(form.prompt && form.photo) {
+      setLoading(true)
+
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/posts', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(form)
+        })
+
+        await response.json()
+        navigate('/')
+
+      } catch (error) {
+        alert(error)
+        console.log(error)
+      } finally {
+        setLoading(false)
+      }
+    } else {
+      alert('Please generate an image first')
+    }
   }
 
   const handleChange = (e) => {
